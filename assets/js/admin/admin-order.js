53c1e221852186a1c9b43d8e3715744339a5ca8d
malsubrata <m.subrata1991@gmail.com>
2018-07-18 19:27:47 +0530

    Merging from branch 1.2.0

--- assets/js/admin/admin-order.js
+++ assets/js/admin/admin-order.js
@@ -3,9 +3,6 @@
 jQuery(function ($) {
     var woo_wallet_order_items = {
         init: function () {
-            if (woo_wallet_admin_order_param.payment_method === 'wallet') {
-                $('.refund-actions .button.tips.disabled').remove();
-            }
             if (woo_wallet_admin_order_param.is_refundable) {
                 $('.refund-actions .do-manual-refund').before('<button type="button" class="button button-primary do-wallet-refund">'+ woo_wallet_admin_order_param.i18n.refund+' <span class="wc-order-refund-amount">' + woo_wallet_admin_order_param.default_price + '</span> '+ woo_wallet_admin_order_param.i18n.via_wallet+'</button>');
                 $('#woocommerce-order-items').on('click', '.refund-actions .do-wallet-refund', this.do_wallet_refund);
