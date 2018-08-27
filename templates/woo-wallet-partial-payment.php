c989fa3bbe2a5844e7eccee4953c07d177d58dab
malsubrata <m.subrata1991@gmail.com>
2018-08-27 17:50:58 +0530

    Fix partial payment amount

--- templates/woo-wallet-partial-payment.php
+++ templates/woo-wallet-partial-payment.php
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 $current_wallet_amount = apply_filters('woo_wallet_partial_payment_amount', woo_wallet()->wallet->get_wallet_balance(get_current_user_id(), 'edit'));
-$rest_amount = wc()->cart->get_total('edit') - $current_wallet_amount;
+$rest_amount = get_woowallet_cart_total() - $current_wallet_amount;
 if ('on' === woo_wallet()->settings_api->get_option('is_auto_deduct_for_partial_payment', '_wallet_settings_general')) {
     ?>
     <tr class="wallet-pay-partial">
