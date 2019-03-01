f7e43e594701db3696f7208b9a4a5257e8ba80a0
malsubrata <m.subrata1991@gmail.com>
2019-03-01 21:14:37 +0530

    Changed woowallet my-account menu nav

--- templates/wc-endpoint-wallet.php
+++ templates/wc-endpoint-wallet.php
@@ -20,22 +20,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $wp;
 do_action( 'woo_wallet_before_my_wallet_content' );
 $is_rendred_from_myaccount = wc_post_content_has_shortcode( 'woo-wallet' ) ? false : is_account_page();
+$menu_items = apply_filters('woo_wallet_nav_menu_items', array(
+    'top_up' => array(
+        'title' => apply_filters( 'woo_wallet_account_topup_menu_title', __( 'Wallet topup', 'woo-wallet' ) ),
+        'url' => $is_rendred_from_myaccount ? esc_url(wc_get_endpoint_url(get_option('woocommerce_woo_wallet_endpoint', 'woo-wallet'), 'add', wc_get_page_permalink('myaccount'))) : add_query_arg('wallet_action', 'add', get_permalink()),
+        'icon' => 'dashicons dashicons-plus-alt'
+    ),
+    'transfer' => array(
+        'title' => apply_filters('woo_wallet_account_transfer_amount_menu_title', __('Wallet transfer', 'woo-wallet')),
+        'url' => $is_rendred_from_myaccount ? esc_url(wc_get_endpoint_url(get_option('woocommerce_woo_wallet_endpoint', 'woo-wallet'), 'transfer', wc_get_page_permalink('myaccount'))) : add_query_arg('wallet_action', 'transfer', get_permalink()),
+        'icon' => 'dashicons dashicons-randomize'
+    ),
+    'transaction_details' => array(
+        'title' => apply_filters('woo_wallet_account_transaction_menu_title', __('Transactions', 'woo-wallet')),
+        'url' => $is_rendred_from_myaccount ? esc_url(wc_get_account_endpoint_url(get_option('woocommerce_woo_wallet_transactions_endpoint', 'woo-wallet-transactions'))) : add_query_arg('wallet_action', 'view_transactions', get_permalink()),
+        'icon' => 'dashicons dashicons-list-view'
+    )
+), $is_rendred_from_myaccount);
 ?>
 
 <div class="woo-wallet-my-wallet-container">
     <div class="woo-wallet-sidebar">
         <h3 class="woo-wallet-sidebar-heading"><a href="<?php echo $is_rendred_from_myaccount ? esc_url( wc_get_account_endpoint_url( get_option( 'woocommerce_woo_wallet_endpoint', 'woo-wallet' ) ) ) : get_permalink(); ?>"><?php echo apply_filters( 'woo_wallet_account_menu_title', __( 'My Wallet', 'woo-wallet' ) ); ?></a></h3>
         <ul>
-            <?php if ( apply_filters( 'woo_wallet_is_enable_top_up', true ) ) : ?>
-                <li class="card"><a href="<?php echo $is_rendred_from_myaccount ? esc_url( wc_get_endpoint_url( get_option( 'woocommerce_woo_wallet_endpoint', 'woo-wallet' ), 'add', wc_get_page_permalink( 'myaccount' ) ) ) : add_query_arg( 'wallet_action', 'add', get_permalink() ); ?>" ><span class="dashicons dashicons-plus-alt"></span><p><?php echo apply_filters( 'woo_wallet_account_topup_menu_title', __( 'Wallet topup', 'woo-wallet' ) ); ?></p></a></li>
-            <?php endif; ?>
-            <?php if ( apply_filters( 'woo_wallet_is_enable_transfer', 'on' === woo_wallet()->settings_api->get_option( 'is_enable_wallet_transfer', '_wallet_settings_general', 'on' ) ) ) : ?>
-                <li class="card"><a href="<?php echo $is_rendred_from_myaccount ? esc_url( wc_get_endpoint_url( get_option( 'woocommerce_woo_wallet_endpoint', 'woo-wallet' ), 'transfer', wc_get_page_permalink( 'myaccount' ) ) ) : add_query_arg( 'wallet_action', 'transfer', get_permalink() ); ?>" ><span class="dashicons dashicons-randomize"></span><p><?php echo apply_filters( 'woo_wallet_account_transfer_amount_menu_title', __( 'Wallet transfer', 'woo-wallet' ) ); ?></p></a></li>
-            <?php endif; ?>
-            <?php if ( apply_filters( 'woo_wallet_is_enable_transaction_details', true ) ) : ?>
-                <li class="card"><a href="<?php echo $is_rendred_from_myaccount ? esc_url( wc_get_account_endpoint_url( get_option( 'woocommerce_woo_wallet_transactions_endpoint', 'woo-wallet-transactions' ) ) ) : add_query_arg( 'wallet_action', 'view_transactions', get_permalink() ); ?>"><span class="dashicons dashicons-list-view"></span><p><?php echo apply_filters( 'woo_wallet_account_transaction_menu_title', __( 'Transactions', 'woo-wallet' ) ); ?></p></a></li>
-            <?php endif; ?>
-            <?php do_action( 'woo_wallet_menu_items' ); ?>
+            <?php foreach ($menu_items as $item => $menu_item) : ?>
+                <?php if (apply_filters('woo_wallet_is_enable_' . $item, true)) : ?>
+                    <li class="card"><a href="<?php echo $menu_item['url']; ?>" ><span class="<?php echo $menu_item['icon'] ?>"></span><p><?php echo $menu_item['title']; ?></p></a></li>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            <?php do_action('woo_wallet_menu_items'); ?>
         </ul>
     </div>
     <div class="woo-wallet-content">
