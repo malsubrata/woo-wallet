9de4948ef5a09a13282025219d43a7b5a189a24a
malsubrata <m.subrata1991@gmail.com>
2022-11-07 21:27:24 -0800

    Fix coding standard

--- includes/class-woo-wallet-ajax.php
+++ includes/class-woo-wallet-ajax.php
@@ -86,18 +86,18 @@ if ( ! class_exists( 'Woo_Wallet_Ajax' ) ) {
 		public function terawallet_do_ajax_transaction_export() {
 			check_ajax_referer( 'terawallet-exporter-script', 'security' );
 			include_once WOO_WALLET_ABSPATH . 'includes/export/class-terawallet-csv-exporter.php';
-			$step = isset( $_POST['step'] ) ? absint( $_POST['step'] ) : 1; // WPCS: input var ok, sanitization ok.
+			$step = isset( $_POST['step'] ) ? absint( $_POST['step'] ) : 1;
 
 			$exporter = new TeraWallet_CSV_Exporter();
 
 			$exporter->set_step( $step );
 
-			if ( ! empty( $_POST['selected_columns'] ) ) { // WPCS: input var ok.
-				$exporter->set_columns_to_export( wp_unslash( $_POST['selected_columns'] ) ); // WPCS: input var ok, sanitization ok.
+			if ( ! empty( $_POST['selected_columns'] ) ) {
+				$exporter->set_columns_to_export( wp_unslash( $_POST['selected_columns'] ) ); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 			}
 
-			if ( ! empty( $_POST['selected_users'] ) ) { // WPCS: input var ok.
-				$exporter->set_users_to_export( wp_unslash( $_POST['selected_users'] ) ); // WPCS: input var ok, sanitization ok.
+			if ( ! empty( $_POST['selected_users'] ) ) {
+				$exporter->set_users_to_export( wp_unslash( $_POST['selected_users'] ) ); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 			}
 
 			if ( ! empty( $_POST['start_date'] ) ) {
@@ -108,8 +108,8 @@ if ( ! class_exists( 'Woo_Wallet_Ajax' ) ) {
 				$exporter->set_end_date( sanitize_text_field( wp_unslash( $_POST['end_date'] ) ) );
 			}
 
-			if ( ! empty( $_POST['filename'] ) ) { // WPCS: input var ok.
-				$exporter->set_filename( wp_unslash( $_POST['filename'] ) ); // WPCS: input var ok, sanitization ok.
+			if ( ! empty( $_POST['filename'] ) ) {
+				$exporter->set_filename( sanitize_text_field( wp_unslash( $_POST['filename'] ) ) );
 			}
 			$exporter->write_to_csv();
 			$query_args = array(
@@ -205,8 +205,7 @@ if ( ! class_exists( 'Woo_Wallet_Ajax' ) ) {
 		/**
 		 * Process refund through wallet
 		 *
-		 * @throws exception WooCommerce exceptions.
-		 * @throws Exception WooCommerce exceptions.
+		 * @throws Exception To return errors.
 		 */
 		public function woo_wallet_order_refund() {
 			ob_start();
@@ -214,27 +213,32 @@ if ( ! class_exists( 'Woo_Wallet_Ajax' ) ) {
 			if ( ! current_user_can( 'edit_shop_orders' ) ) {
 				wp_die( -1 );
 			}
-			$order_id               = absint( $_POST['order_id'] );
-			$refund_amount          = wc_format_decimal( sanitize_text_field( $_POST['refund_amount'] ), wc_get_price_decimals() );
-			$refund_reason          = sanitize_text_field( $_POST['refund_reason'] );
-			$line_item_qtys         = json_decode( sanitize_text_field( stripslashes( $_POST['line_item_qtys'] ) ), true );
-			$line_item_totals       = json_decode( sanitize_text_field( stripslashes( $_POST['line_item_totals'] ) ), true );
-			$line_item_tax_totals   = json_decode( sanitize_text_field( stripslashes( $_POST['line_item_tax_totals'] ) ), true );
-			$api_refund             = 'true' === $_POST['api_refund'];
-			$restock_refunded_items = 'true' === $_POST['restock_refunded_items'];
+			$order_id               = isset( $_POST['order_id'] ) ? absint( $_POST['order_id'] ) : 0;
+			$refund_amount          = isset( $_POST['refund_amount'] ) ? wc_format_decimal( sanitize_text_field( wp_unslash( $_POST['refund_amount'] ) ), wc_get_price_decimals() ) : 0;
+			$refunded_amount        = isset( $_POST['refunded_amount'] ) ? wc_format_decimal( sanitize_text_field( wp_unslash( $_POST['refunded_amount'] ) ), wc_get_price_decimals() ) : 0;
+			$refund_reason          = isset( $_POST['refund_reason'] ) ? sanitize_text_field( wp_unslash( $_POST['refund_reason'] ) ) : '';
+			$line_item_qtys         = isset( $_POST['line_item_qtys'] ) ? json_decode( sanitize_text_field( wp_unslash( $_POST['line_item_qtys'] ) ), true ) : array();
+			$line_item_totals       = isset( $_POST['line_item_totals'] ) ? json_decode( sanitize_text_field( wp_unslash( $_POST['line_item_totals'] ) ), true ) : array();
+			$line_item_tax_totals   = isset( $_POST['line_item_tax_totals'] ) ? json_decode( sanitize_text_field( wp_unslash( $_POST['line_item_tax_totals'] ) ), true ) : array();
+			$api_refund             = isset( $_POST['api_refund'] ) && 'true' === $_POST['api_refund'];
+			$restock_refunded_items = isset( $_POST['restock_refunded_items'] ) && 'true' === $_POST['restock_refunded_items'];
 			$refund                 = false;
-			$response_data          = array();
+			$response               = array();
 			try {
-				$order       = wc_get_order( $order_id );
-				$order_items = $order->get_items();
-				$max_refund  = wc_format_decimal( $order->get_total() - $order->get_total_refunded(), wc_get_price_decimals() );
+				$order      = wc_get_order( $order_id );
+				$max_refund = wc_format_decimal( $order->get_total() - $order->get_total_refunded(), wc_get_price_decimals() );
 
-				if ( ! $refund_amount || $max_refund < $refund_amount || 0 > $refund_amount ) {
-					throw new exception( __( 'Invalid refund amount', 'woo-wallet' ) );
+				if ( ( ! $refund_amount && ( wc_format_decimal( 0, wc_get_price_decimals() ) !== $refund_amount ) ) || $max_refund < $refund_amount || 0 > $refund_amount ) {
+					throw new Exception( __( 'Invalid refund amount', 'woocommerce' ) );
 				}
+
+				if ( wc_format_decimal( $order->get_total_refunded(), wc_get_price_decimals() ) !== $refunded_amount ) {
+					throw new Exception( __( 'Error processing refund. Please try again.', 'woocommerce' ) );
+				}
+
 				// Prepare line items which we are refunding.
 				$line_items = array();
-				$item_ids   = array_unique( array_merge( array_keys( $line_item_qtys, $line_item_totals ) ) );
+				$item_ids   = array_unique( array_merge( array_keys( $line_item_qtys ), array_keys( $line_item_totals ) ) );
 
 				foreach ( $item_ids as $item_id ) {
 					$line_items[ $item_id ] = array(
@@ -252,6 +256,7 @@ if ( ! class_exists( 'Woo_Wallet_Ajax' ) ) {
 				foreach ( $line_item_tax_totals as $item_id => $tax_totals ) {
 					$line_items[ $item_id ]['refund_tax'] = array_filter( array_map( 'wc_format_decimal', $tax_totals ) );
 				}
+
 				// Create the refund object.
 				$refund = wc_create_refund(
 					array(
@@ -277,16 +282,13 @@ if ( ! class_exists( 'Woo_Wallet_Ajax' ) ) {
 				}
 
 				if ( did_action( 'woocommerce_order_fully_refunded' ) ) {
-					$response_data['status'] = 'fully_refunded';
+					$response['status'] = 'fully_refunded';
 				}
-
-				wp_send_json_success( $response_data );
-			} catch ( Exception $ex ) {
-				if ( $refund && is_a( $refund, 'WC_Order_Refund' ) ) {
-					wp_delete_post( $refund->get_id(), true );
-				}
-				wp_send_json_error( array( 'error' => $ex->getMessage() ) );
+			} catch ( Exception $e ) {
+				wp_send_json_error( array( 'error' => $e->getMessage() ) );
 			}
+			// wp_send_json_success must be outside the try block not to break phpunit tests.
+			wp_send_json_success( $response );
 		}
 
 		/**
@@ -304,9 +306,10 @@ if ( ! class_exists( 'Woo_Wallet_Ajax' ) ) {
 		 */
 		public function woo_wallet_user_search() {
 			$return = array();
+			$term = isset( $_POST['term'] ) ? sanitize_text_field( wp_unslash( $_POST['term'] ) ) : '';
 			if ( apply_filters( 'woo_wallet_user_search_exact_match', true ) ) {
-				$user = get_user_by( apply_filters( 'woo_wallet_user_search_by', 'email' ), $_REQUEST['term'] ); // phpcs:ignore
-				if ( $user && wp_get_current_user()->user_email != $user->user_email ) {
+				$user = get_user_by( apply_filters( 'woo_wallet_user_search_by', 'email' ), $term );
+				if ( $user && wp_get_current_user()->user_email !== $user->user_email ) {
 					$return[] = array(
 						/* translators: 1: user_login, 2: user_email */
 						'label' => sprintf( _x( '%1$s (%2$s)', 'user autocomplete result', 'woo-wallet' ), $user->user_login, $user->user_email ),
@@ -314,16 +317,12 @@ if ( ! class_exists( 'Woo_Wallet_Ajax' ) ) {
 					);
 				}
 			} else {
-				if ( isset( $_REQUEST['site_id'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-					$id = absint( $_REQUEST['site_id'] ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-				} else {
-					$id = get_current_blog_id();
-				}
+				$blog_id = isset( $_POST['site_id'] ) ? sanitize_text_field( wp_unslash( $_POST['site_id'] ) ) : get_current_blog_id();
 
 				$users = get_users(
 					array(
-						'blog_id'        => $id,
-						'search'         => '*' . $_REQUEST['term'] . '*', // phpcs:ignore
+						'blog_id'        => $blog_id,
+						'search'         => '*' . $term . '*',
 						'exclude'        => array( get_current_user_id() ),
 						'search_columns' => array( 'user_login', 'user_nicename', 'user_email' ),
 					)
@@ -375,9 +374,9 @@ if ( ! class_exists( 'Woo_Wallet_Ajax' ) ) {
 		 */
 		public function draw_wallet_transaction_details_table() {
 			check_ajax_referer( 'woo-wallet-transactions', 'security' );
-			$start  = isset( $_POST['start'] ) ? $_POST['start'] : 0; // phpcs:ignore
-			$length = isset( $_POST['length'] ) ? $_POST['length'] : 10; // phpcs:ignore
-			$search = $_POST['search']; // phpcs:ignore
+			$start  = isset( $_POST['start'] ) ? sanitize_text_field( wp_unslash( $_POST['start'] ) ) : 0;
+			$length = isset( $_POST['length'] ) ? sanitize_text_field( wp_unslash( $_POST['length'] ) ) : 10;
+			$search = isset( $_POST['search'] ) ? sanitize_text_field( wp_unslash( $_POST['search'] ) ) : '';
 			$args   = array(
 				'limit' => "$start, $length",
 			);
@@ -395,7 +394,7 @@ if ( ! class_exists( 'Woo_Wallet_Ajax' ) ) {
 			$records_total = get_wallet_transactions_count( get_current_user_id() );
 
 			$response = array(
-				'draw'            => $_POST['draw'], // phpcs:ignore
+				'draw'            => isset( $_POST['draw'] ) ? sanitize_text_field( wp_unslash( $_POST['draw'] ) ) : 1,
 				'recordsTotal'    => $records_total,
 				'recordsFiltered' => count( get_wallet_transactions( $args ) ),
 				'data'            => array(),
